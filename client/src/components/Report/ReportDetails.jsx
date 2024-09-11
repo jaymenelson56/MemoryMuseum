fc981b84aa0a0646c9b29b9bf8864799b2ba75e5
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-09-11 00:13:04 -0500

    added more to user details, fixed an error in registraion, sorted reports  in controller

--- client/src/components/Report/ReportDetails.jsx
+++ client/src/components/Report/ReportDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
+  Button,
   Card,
   CardBody,
   CardHeader,
@@ -11,25 +12,41 @@ import { getReport } from "../../managers/reportmanager";
 
 export const ReportDetails = () => {
   const [report, setReport] = useState({});
-  const { id } =useParams();
-  
-  useEffect (() => {
+  const { id } = useParams();
+
+  useEffect(() => {
     getReport(id).then(setReport);
-  }, [id])
+  }, [id]);
 
   return (
     <>
       <Card className="transparent-card">
         <h2>Report</h2>
         <Card>
+          {report?.closed ? (
+            <div
+              style={{ padding: "10px", textAlign: "center", color: "green" }}
+            >
+              <strong>Issue resolved</strong>
+            </div>
+          ) : (
+            <Button color="info">Close Ticket</Button>
+          )}
           <CardBody>
-            <CardHeader>Subject: <b>{report?.reportSubject}</b></CardHeader>
+            <CardHeader>
+              Subject: <b>{report?.reportSubject}</b>
+            </CardHeader>
 
             <ListGroup flush>
-              <ListGroupItem>Author: <b>{report?.reportAuthor}</b></ListGroupItem>
-              <ListGroupItem>Issue: <b>{report?.body}</b></ListGroupItem>
-              <ListGroupItem>Status: <b>{report?.closed ? "Closed" : "Open"}</b></ListGroupItem>
-              <ListGroupItem>Action</ListGroupItem>
+              <ListGroupItem>
+                Author: <b>{report?.reportAuthor}</b>
+              </ListGroupItem>
+              <ListGroupItem>
+                Issue: <b>{report?.body}</b>
+              </ListGroupItem>
+              <ListGroupItem>
+                Status: <b>{report?.closed ? "Closed" : "Open"}</b>
+              </ListGroupItem>
             </ListGroup>
           </CardBody>
         </Card>
