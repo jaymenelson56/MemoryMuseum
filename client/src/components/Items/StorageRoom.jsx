e960011f4f965100e0f8c1b96a99d628046058b7
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-09-27 21:51:03 -0500

    rehauled storage room

--- client/src/components/Items/StorageRoom.jsx
+++ client/src/components/Items/StorageRoom.jsx
@@ -1,122 +1,228 @@
 import { useEffect, useState } from "react";
-import { Card, CardBody, CardFooter, CardText, CardTitle, CardImg, CardHeader, Button } from "reactstrap";
-import { approveItem, deleteItem, getNotApprovedItems, getPendingItems, getRejectedItems, rejectItem } from "../../managers/itemManager";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardText,
+  CardTitle,
+  CardImg,
+  CardHeader,
+  Button,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+import {
+  approveItem,
+  changeExhibit,
+  deleteItem,
+  getNotApprovedItems,
+  getPendingItems,
+  getRejectedItems,
+  rejectItem,
+} from "../../managers/itemManager";
 import { Link } from "react-router-dom";
+import { getExhibits } from "../../managers/exhibitManager";
 
 export const StorageRoom = ({ loggedInUser }) => {
-    const [pendingItems, setPendingItems] = useState([]);
-    const [rejectedItems, setRejectedItems] = useState([]);
-    const [notApprovedItems, setNotApprovedItems] = useState([]);
+  const [pendingItems, setPendingItems] = useState([]);
+  const [rejectedItems, setRejectedItems] = useState([]);
+  const [notApprovedItems, setNotApprovedItems] = useState([]);
+  const [exhibits, setExhibits] = useState([]);
+  const [exhibitId, setExhibitId] = useState("0");
 
-    useEffect(() => {
-        getPendingItems(loggedInUser.id).then(setPendingItems);
-        getRejectedItems(loggedInUser.id).then(setRejectedItems);
-        getNotApprovedItems(loggedInUser.id).then(setNotApprovedItems);
-    }, [loggedInUser]);
+  useEffect(() => {
+    getPendingItems(loggedInUser.id).then(setPendingItems);
+    getRejectedItems(loggedInUser.id).then(setRejectedItems);
+    getNotApprovedItems(loggedInUser.id).then(setNotApprovedItems);
+    getExhibits().then(setExhibits);
+  }, [loggedInUser]);
 
-    const handleApprove = async (id) => {
-        await approveItem(id);
-        setPendingItems(pendingItems.filter(item => item.id !== id));
-    };
+  const handleApprove = async (id) => {
+    await approveItem(id);
+    setPendingItems(pendingItems.filter((item) => item.id !== id));
+  };
 
-    const handleReject = async (id) => {
-        await rejectItem(id);
-        setPendingItems(pendingItems.filter(item => item.id !== id));
-        setRejectedItems(await getRejectedItems(loggedInUser.id));
-    };
+  const handleReject = async (id) => {
+    await rejectItem(id);
+    setPendingItems(pendingItems.filter((item) => item.id !== id));
+    setRejectedItems(await getRejectedItems(loggedInUser.id));
+  };
 
-    const handleDelete = async (id) => {
-        await deleteItem(id);
-        setRejectedItems(rejectedItems.filter(item => item.id !== id));
-    };
+  const handleDelete = async (id) => {
+    await deleteItem(id);
+    setRejectedItems(rejectedItems.filter((item) => item.id !== id));
+    setNotApprovedItems(notApprovedItems.filter((item) => item.id !== id));
+  };
 
-    return (
-        <>
-            <Card>
-                <h2>Incoming Items</h2>
-                {pendingItems.length === 0 ? (
-                    <CardBody>
-                        <CardText>No incoming items at this time.</CardText>
-                    </CardBody>
-                ) : (
-                    pendingItems.map((item) => (
-                        <Card key={item.id}>
-                            <CardBody>
-                                <CardHeader>Item: {item.name}</CardHeader>
-                                <CardImg src={item.image} alt={item.name} style={{
-                                    width: '18rem'
-                                }} />
-                                <CardTitle>For Exhibit: {item.exhibit.name}</CardTitle>
-                                <CardTitle>Original Owner: <Link to={`/userprofiles/${item.userProfileId}`}>{item.userProfile.userName}</Link></CardTitle>
-                                <CardText>Description: {item.placard}</CardText>
-                                <CardFooter>
-                                    <Button color="success" onClick={() => handleApprove(item.id)}>Approve</Button>
-                                    <Button color="danger" onClick={() => handleReject(item.id)}>Reject</Button></CardFooter>
-                            </CardBody>
-                        </Card>
-                    ))
-                )}
+  const handleChangeExhibit = async (itemId) => {
+    try {
+      await changeExhibit(itemId, exhibitId, loggedInUser.id);
+      // Optionally refresh the rejected items after changing the exhibit
+      setRejectedItems(await getRejectedItems(loggedInUser.id));
+      setNotApprovedItems(await getNotApprovedItems(loggedInUser.id));
+      // Reset exhibitId after the operation
+      setExhibitId("0");
+    } catch (error) {
+      console.error("Failed to change exhibit:", error);
+    }
+  };
+
+  return (
+    <>
+      <Card>
+        <h2>Incoming Items</h2>
+        {pendingItems.length === 0 ? (
+          <CardBody>
+            <CardText>No incoming items at this time.</CardText>
+          </CardBody>
+        ) : (
+          pendingItems.map((item) => (
+            <Card key={item.id}>
+              <CardBody>
+                <CardHeader>Item: {item.name}</CardHeader>
+                <CardImg
+                  src={item.image}
+                  alt={item.name}
+                  style={{
+                    width: "18rem",
+                  }}
+                />
+                <CardTitle>For Exhibit: {item.exhibit.name}</CardTitle>
+                <CardTitle>
+                  Original Owner:{" "}
+                  <Link to={`/userprofiles/${item.userProfileId}`}>
+                    {item.userProfile.userName}
+                  </Link>
+                </CardTitle>
+                <CardText>Description: {item.placard}</CardText>
+                <CardFooter>
+                  <Button
+                    color="success"
+                    onClick={() => handleApprove(item.id)}
+                  >
+                    Approve
+                  </Button>
+                  <Button color="danger" onClick={() => handleReject(item.id)}>
+                    Reject
+                  </Button>
+                </CardFooter>
+              </CardBody>
             </Card>
-            <Card>
-                <h2>Outgoing Items</h2>
-                {rejectedItems.length === 0 ? (
-                    <CardBody>
-                        <CardText>No outgoing items at this time.</CardText>
-                    </CardBody>
-                ) : (
-                    notApprovedItems.map((item) => (
-                        <Card key={item.id}>
-                            <CardBody>
-                                <CardHeader>Item: {item.name}</CardHeader>
-                                <CardImg src={item.image} alt={item.name} style={{
-                                    width: '18rem'
-                                }} />
-                                <CardTitle>For Exhibit: {item.exhibit.name}</CardTitle>
-                                <CardText>
-                                    Status: {item.needsApproval
-                                        ? 'Pending'
-                                        : <>Rejected by <Link to={`/userprofiles/${item.exhibit.userProfileId}`}>{item.exhibit.userProfile.userName}</Link></>
-                                    }
-                                </CardText>
-                                <CardText>Description: {item.placard}</CardText>
-                                <CardFooter>
-                                    <Button color="danger" onClick={() => handleDelete(item.id)}>Delete</Button>
-                                </CardFooter>
-                            </CardBody>
-                        </Card>
-                    ))
-                )}
+          ))
+        )}
+      </Card>
+      <Card>
+        <h2>Outgoing Items</h2>
+        {notApprovedItems.length === 0 ? (
+          <CardBody>
+            <CardText>No outgoing items at this time.</CardText>
+          </CardBody>
+        ) : (
+          notApprovedItems.map((item) => (
+            <Card key={item.id}>
+              <CardBody>
+                <CardHeader>Item: {item.name}</CardHeader>
+                <CardImg
+                  src={item.image}
+                  alt={item.name}
+                  style={{
+                    width: "18rem",
+                  }}
+                />
+                <CardTitle>For Exhibit: {item.exhibit.name}</CardTitle>
+                <CardText>
+                  Status:{" "}
+                  {item.needsApproval ? (
+                    "Pending"
+                  ) : (
+                    <>
+                      Rejected by{" "}
+                      <Link to={`/userprofiles/${item.exhibit.userProfileId}`}>
+                        {item.exhibit.userProfile.userName}
+                      </Link>
+                    </>
+                  )}
+                </CardText>
+                <CardText>Description: {item.placard}</CardText>
+                <CardFooter>
+                  <Button color="danger" onClick={() => handleDelete(item.id)}>
+                    Delete
+                  </Button>
+                </CardFooter>
+              </CardBody>
             </Card>
-            <Card>
-                <h2>Rejected Items</h2>
-                {rejectedItems.length === 0 ? (
-                    <CardBody>
-                        <CardText>No Rejected items at this time.</CardText>
-                    </CardBody>
-                ) : (
-                    rejectedItems.map((item) => (
-                        <Card key={item.id}>
-                            <CardBody>
-                                <CardHeader>Item: {item.name}</CardHeader>
-                                <CardImg src={item.image} alt={item.name} style={{
-                                    width: '18rem'
-                                }} />
-                                <CardTitle>For Exhibit: {item.exhibit.name}</CardTitle>
-                                <CardText>
-                                    Status: {item.needsApproval
-                                        ? 'Pending'
-                                        : <>Rejected by <Link to={`/userprofiles/${item.exhibit.userProfileId}`}>{item.exhibit.userProfile.userName}</Link></>
-                                    }
-                                </CardText>
-                                <CardText>Description: {item.placard}</CardText>
-                                <CardFooter>
-                                    <Button color="danger" onClick={() => handleDelete(item.id)}>Delete</Button>
-                                </CardFooter>
-                            </CardBody>
-                        </Card>
-                    ))
-                )}
+          ))
+        )}
+      </Card>
+      <Card>
+        <h2>Rejected Items</h2>
+        {rejectedItems.length === 0 ? (
+          <CardBody>
+            <CardText>No Rejected items at this time.</CardText>
+          </CardBody>
+        ) : (
+          rejectedItems.map((item) => (
+            <Card key={item.id}>
+              <CardBody>
+                <CardHeader>Item: {item.name}</CardHeader>
+                <CardImg
+                  src={item.image}
+                  alt={item.name}
+                  style={{
+                    width: "18rem",
+                  }}
+                />
+                <CardTitle>For Exhibit: {item.exhibit.name}</CardTitle>
+                <CardText>
+                  Status:{" "}
+                  {item.needsApproval ? (
+                    "Pending"
+                  ) : (
+                    <>
+                      Rejected by{" "}
+                      <Link to={`/userprofiles/${item.exhibit.userProfileId}`}>
+                        {item.exhibit.userProfile.userName}
+                      </Link>
+                    </>
+                  )}
+                </CardText>
+                <CardText>Description: {item.placard}</CardText>
+                <FormGroup>
+                  <Label htmlFor="exhibit">Exhibit</Label>
+                  <Input
+                    type="select"
+                    id="exhibit"
+                    value={exhibitId}
+                    onChange={(e) => {
+                      setExhibitId(parseInt(e.target.value));
+                    }}
+                  >
+                    <option value={0}>Send to another Exhibit</option>
+                    {exhibits.map((ex) => (
+                      <option key={ex.id} value={ex.id}>
+                        {ex.name}
+                      </option>
+                    ))}
+                  </Input>
+                </FormGroup>
+                <CardFooter>
+                  <Button
+                    color="primary"
+                    onClick={() => handleChangeExhibit(item.id)}
+                    disabled={exhibitId === "0"}
+                  >
+                    Change Exhibit
+                  </Button>
+                  <Button color="danger" onClick={() => handleDelete(item.id)}>
+                    Delete
+                  </Button>
+                </CardFooter>
+              </CardBody>
             </Card>
-        </>
-    );
+          ))
+        )}
+      </Card>
+    </>
+  );
 };
--- client/src/managers/itemManager.js
+++ client/src/managers/itemManager.js
@@ -1,62 +1,81 @@
 const _apiUrl = "/api/Item";
 
 export const newItem = async (itemData) => {
-    const response = await fetch(_apiUrl, {
-        method: 'POST',
-        body: itemData
-    });
-    const data = await response.json();
-    return data
-}
+  const response = await fetch(_apiUrl, {
+    method: "POST",
+    body: itemData,
+  });
+  const data = await response.json();
+  return data;
+};
 
 export const getItem = async (id) => {
-    return await fetch(_apiUrl + `/${id}`).then((res) => res.json());
+  return await fetch(_apiUrl + `/${id}`).then((res) => res.json());
 };
 
 export const updateItem = async (id, itemData) => {
-    return fetch(_apiUrl + `/${id}`, {
-        method: "PUT",
-        body: itemData,
-    }).then((res) => res.json());
-}
+  return fetch(_apiUrl + `/${id}`, {
+    method: "PUT",
+    body: itemData,
+  }).then((res) => res.json());
+};
 
 export const deleteItem = async (id) => {
-    const response = await fetch(_apiUrl + `/${id}`, {
-        method: 'DELETE'
-    });
-    if (!response.ok) {
-        throw new Error('Failed to delete comment');
-    }
-}
+  const response = await fetch(_apiUrl + `/${id}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to delete comment");
+  }
+};
 
 export const getPendingItems = async (id) => {
-    return await fetch(_apiUrl + `/needing-approval/${id}`).then((res) => res.json());
+  return await fetch(_apiUrl + `/needing-approval/${id}`).then((res) =>
+    res.json()
+  );
 };
 
 export const getRejectedItems = async (id) => {
-    return await fetch(_apiUrl + `/rejected/${id}`).then((res) => res.json());
+  return await fetch(_apiUrl + `/rejected/${id}`).then((res) => res.json());
 };
 
 export const getNotApprovedItems = async (id) => {
-    return await fetch(_apiUrl + `/not-approved/${id}`).then((res) => res.json());
+  return await fetch(_apiUrl + `/not-approved/${id}`).then((res) => res.json());
 };
 
 export const approveItem = async (id) => {
-    const response = await fetch(_apiUrl + `/approve/${id}`, {
-        method: 'PUT'
-    });
-    if (!response.ok) {
-        throw new Error('Failed to approve item');
-    }
-    return response.ok;
+  const response = await fetch(_apiUrl + `/approve/${id}`, {
+    method: "PUT",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to approve item");
+  }
+  return response.ok;
 };
 
 export const rejectItem = async (id) => {
-    const response = await fetch(_apiUrl + `/reject/${id}`, {
-        method: 'PUT'
-    });
-    if (!response.ok) {
-        throw new Error('Failed to reject item');
-    }
-    return response.ok;
+  const response = await fetch(_apiUrl + `/reject/${id}`, {
+    method: "PUT",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to reject item");
+  }
+  return response.ok;
+};
+
+export const changeExhibit = async (id, newExhibitId, loggedInUserId) => {
+  const formData = new FormData();
+  formData.append("newExhibitId", newExhibitId);
+  formData.append("loggedInUserId", loggedInUserId);
+
+  const response = await fetch(`${_apiUrl}/change-exhibit/${id}`, {
+    method: "PUT",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to change exhibit");
+  }
+
+  return response.ok;
 };
